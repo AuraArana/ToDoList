5037f68727e9e5d352667175c99925a0d66b3544
Aura Arana <Aura.Arana001@mymdc.net>
2021-03-01 23:03:15 +0000

    added todos

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [todo, setTodo] = useState("");
+	const [list, setList] = useState([]);
+
 	return (
 		<div className="text-center mt-5">
-			<inout
-				placeholder="todos"
-				onChange={e => setTodos(e.target.value)}></inout>
-			<button>Add ToDo</button>
-		</div>
-	);
-}
+            <div>
+                <h1>To Do's</h1>
+                <input onChange={e => setTodo(e.target.value)} value={todo} />
+			    <button
+				onClick={() => {
+					setList([...list, todo]);
+					setTodo("");
+				}}>
+				Add
+			    </button>
+			{list.map((item, index) => {
+                <div 
+                    key={index}
+                    className="row"
+                    onMouseEnter={() => setTrash(!trash)}
+                    onMouseLeave={() => setTrash(!trash)}>
+                    <div className="d-flex justify-content-end col">
+                        <p>{item}</p>
+                    </div>
+                    <div class="d-flex justify-content-end col">
+                        {!trash === false ? ( 
+                    <button>
+                        <i class="fas fa-times"></i>
+                    </button>
+                    ) : ("")}
+                    </div>
+                </div>    
+			})}
+        </div>);  
+
+export default Home;
