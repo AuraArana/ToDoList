d959611d7094f7df808c23086986554285cd72a0
Aura Arana <Aura.Arana001@mymdc.net>
2021-03-08 23:22:44 +0000

    added &

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,11 +8,12 @@ export function Home() {
 	const [todo, setTodo] = useState("");
 	const [list, setList] = useState([]);
 	const [trash, setTrash] = useState(false);
+	console.log(list);
 
 	return (
 		<div className="text-center mt-5">
 			<div>
-				<h1>To Do's</h1>
+				<h1>To Do&apos;s</h1>
 				<input onChange={e => setTodo(e.target.value)} value={todo} />
 				<button
 					onClick={() => {
@@ -21,26 +22,27 @@ export function Home() {
 					}}>
 					Add
 				</button>
-				{list.map((item, index) => {
-					<div
-						key={index}
-						className="container-row"
-						onMouseEnter={() => setTrash(!trash)}
-						onMouseLeave={() => setTrash(!trash)}>
-						<div className="d-flex justify-content-start col">
-							<p>{item}</p>
-						</div>
-						<div className="d-flex justify-content-end col">
-							{trash === false ? (
-								<button>
-									<i className="fas fa-times"></i>
-								</button>
-							) : (
-								""
-							)}
-						</div>
-					</div>;
-				})}
+				{list &&
+					list.map((item, index) => {
+						<div
+							key={index}
+							className="container-row"
+							onMouseEnter={() => setTrash(!trash)}
+							onMouseLeave={() => setTrash(!trash)}>
+							<div className="d-flex justify-content-start col">
+								<p>{item}</p>
+							</div>
+							<div className="d-flex justify-content-end col">
+								{trash === false ? (
+									<button>
+										<i className="fas fa-times"></i>
+									</button>
+								) : (
+									""
+								)}
+							</div>
+						</div>;
+					})}
 			</div>
 		</div>
 	);
